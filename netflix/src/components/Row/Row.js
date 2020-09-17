8b8893b2b1653f211cc7e8592bcb51bbc978a5c0
GoobyTheBOI <luuklentjes2002@gmail.com>
2020-09-17 11:24:23 +0200

    added Netflix slider

--- netflix/src/components/Row/Row.js
+++ netflix/src/components/Row/Row.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../axios";
 import "./row.css";
+import "./swiper.scss";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {Navigation, Pagination} from 'swiper';
+
+SwiperCore.use([Navigation, Pagination]);
 
 const image_url = "https://image.tmdb.org/t/p/original/";
 
@@ -26,19 +31,23 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
             {/* several Posters */}
             <div className="row__posters">
-                {movies.map((movie) => (
-                    // console.log(movie)
-                    <img
-                        className={`row__poster ${
-                            isLargeRow && "row__posterLargeRow"
-                        }`}
-                        key={movie.id}
-                        src={`${image_url}${
-                            isLargeRow ? movie.poster_path : movie.backdrop_path
-                        }`}
-                        alt={movie.name}
-                    />
-                ))}
+                <Swiper slidesPerView={6} spaceBetween={10} navigation pagination slidesPerGroup={4}>
+                    {movies.map((movie) => (
+                        // console.log(movie)
+                        <SwiperSlide key={movie.id}>
+                            <img
+                                className={`row__poster ${
+                                    isLargeRow && "row__posterLargeRow"
+                                }`}
+                                key={movie.id}
+                                src={`${image_url}${
+                                    isLargeRow ? movie.poster_path : movie.backdrop_path
+                                }`}
+                                alt={movie.name}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
 
             {/* container -> posters */}
